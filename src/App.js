0bf4db476204defdbca1d147262b88c0f9986586
Nguyễn Công Anh <31842967+anhcongnguyen@users.noreply.github.com>
2022-04-03 19:34:14 +0700

    done navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import FormRegister from "./components/NavBar/FromRegister/FormRegister";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <FormRegister />
     </>
   );
