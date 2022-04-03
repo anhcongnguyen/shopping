869aef8be1ddd779156a4ae6b23f0e9935acdcd6
Nguyễn Công Anh <31842967+anhcongnguyen@users.noreply.github.com>
2022-04-03 15:44:09 +0700

    done form register

--- src/components/NavBar/FromRegister/FormRegister.jsx
+++ src/components/NavBar/FromRegister/FormRegister.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 
@@ -20,15 +21,15 @@ const FormRegister = () => {
 
   return (
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
-        <div>
+      <div class="max-w-md w-full space-y-8 border rounded-md shadow-xl">
+        <div class="mt-3">
           <img
             class="mx-auto h-12 w-auto"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow"
           />
           <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Create your account
           </h2>
           <p class="mt-2 text-center text-sm text-gray-600">
             Or
@@ -41,7 +42,7 @@ const FormRegister = () => {
             </a>
           </p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} class="mt-8 space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} class="mt-8 space-y-6 m-3">
           <div class="round">
             <Controller
               name="firstName"
@@ -49,13 +50,13 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="First Name"
                   {...field}
                 />
               )}
             />
-            {errors.firstname?.type === "required" && (
+            {errors.firstName?.type === "required" && (
               <span>First Name is required</span>
             )}
             <Controller
@@ -64,13 +65,13 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Last Name"
                   {...field}
                 />
               )}
             />
-            {errors.lastname?.type === "required" && (
+            {errors.lastName?.type === "required" && (
               <span>Last Name is required</span>
             )}
             <Controller
@@ -79,7 +80,7 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email"
                   {...field}
                 />
@@ -94,7 +95,7 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Phone"
                   {...field}
                 />
@@ -109,7 +110,7 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Username"
                   {...field}
                 />
@@ -124,7 +125,7 @@ const FormRegister = () => {
               rules={{ required: true }}
               render={({ field }) => (
                 <input
-                  class="appearance-none rounded-t-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  class="appearance-none rounded-t-lg relative block w-full my-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
                   {...field}
                 />
@@ -134,7 +135,12 @@ const FormRegister = () => {
               <span>Password is required</span>
             )}
           </div>
-          <input type="submit" />
+          <div>
+            <input
+              type="submit"
+              class="appearance-none rounded-md relative block w-full my-2 px-3 py-2 border border-gray-300 bg-blue-400 text-white hover:bg-blue-600"
+            />
+          </div>
         </form>
       </div>
     </div>
