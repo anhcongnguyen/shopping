69d47acefb28797fb70e10f8a39b9dc3d08e8b94
guyenanh <nguyenanhyoung@gmail.com>
2022-04-04 16:51:22 +0700

    Home page inprogress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import FormRegister from "./components/NavBar/FromRegister/FormRegister";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <FormRegister />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../../components/Container/Container'
+import NavBar from '../../components/NavBar/NavBar'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar />
+        <Container/>
+    </div>
+  )
+}
+
+export default Home
